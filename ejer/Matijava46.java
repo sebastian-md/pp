1feddc1707f274341855687f37b98e8ab1fe0139
sebastian-md <sebas.adrv@gmail.com>
2023-03-07 17:01:19 +0100

    v1701

--- ejer/Matijava46.java
+++ ejer/Matijava46.java
@@ -1,7 +1,7 @@
 public class Matijava46 {
 
 	public static void main(String[] args) {		
-		System.out.println("Me llamo Sebastian y he creado el servidor ubunserver46");
+		System.out.println("Me llamo Sebastian y he creado el servidor ubunserver46 y vivo en Turquia");
 	}
 
 }
